ca6b29d5ba5ba49debfb7e56c9f2de1ee0169b38
ILYAYUSUPOV <ilyayusupovilya@gmail.com>
2020-08-03 16:23:38 +0300

    Html 100% , js 100% . Animation not done !

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 const car = (name, model , owner, year, phone, image) => ({name, model , owner, year, phone, image})
-
+const log = (text, type , date = new Date()) => ({text, type , date})
 
 const cars = [
     car('Ford', 'Focus', 'Dima', 2017, '+7 929 123 45 67', 'images/fordfocus.jpg'),
@@ -12,6 +12,46 @@ const cars = [
 new Vue({
     el: '#app',
     data:{
-        cars: cars
+        cars: cars,
+        car: cars[0],
+        selectedCarIndex: 0,
+        phoneVisibility: false,
+        search: '',
+        modalVisibility: false,
+        logs: []
+    },
+    methods: {
+        selectCar: function(index) {
+            this.car = cars[index]
+            this.selectedCarIndex = index
+        },
+        newOrder(){
+            this.modalVisibility = false
+            this.logs.push(
+                log(`Success order: ${this.car.name} - ${this.car.model}`, `ok` )
+            )
+        },
+        cansleOrder(){
+            this.modalVisibility = false
+            this.logs.push(
+                log(`Cancelled order: ${this.car.name} - ${this.car.model}`, `cnl` )
+            )
+        }
+    },
+    computed: {
+        phoneBtnText() {
+            return this.phoneVisibility ? 'Hide phone' : 'Show phone'
+            },
+            filteredCars() {
+                return this.cars.filter(car => {
+                    return car.name.indexOf(this.search) > -1 || car.model.indexOf(this.search) > -1
+                })
+            }
+    },
+    filters: {
+        date(value) {
+            return value.toLocaleString()
+        }
     }
 })
+
